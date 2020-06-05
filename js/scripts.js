e8007cae9106b3db15b9a8f35a7cef7b96244778
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-05 12:27:11 -0700

    add q10 q11 and q12

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,9 @@ $(document).ready(function() {
     const q7Input = parseInt($("input[name='q7']:checked").val());
     const q8Input = parseInt($("input[name='q8']:checked").val());
     const q9Input = parseInt($("input[name='q9']:checked").val());
+    const q10Input = parseInt($("input[name='q10']:checked").val());
+    const q11Input = parseInt($("input[name='q11']:checked").val());
+    const q12Input = parseInt($("input[name='q12']:checked").val());
 
     if (q1Input === 1) {
       cplus += 1;
@@ -129,6 +132,34 @@ $(document).ready(function() {
       cplus += 1;
     }
 
-    
+    if (q10Input === 1) {
+      cplus += 1;
+      csharp += 1;
+      java += 3;
+      javascript += 1;
+      objectiveC += 1;
+      php += 1;
+      python += 1;
+      ruby += 1;
+    } else if (q10Input === 2) {
+      c += 1;
+      cplus += 1;
+      java += 1;
+      python += 2;
+    }
+
+    if (q11Input === 1) {
+      java += 1;
+    } else if (q11Input === 2) {
+      c += 1;
+    }
+
+    if (q12Input === 1) {
+      python += 1;
+    } else if (q12Input === 2) {
+      ruby += 1;
+    } else if (q12Input === 3) {
+      php += 1;
+    }
   });
 })
