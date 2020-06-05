c388d555144677419aec81fe1eb2bd32384ef60f
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-05 09:11:13 -0700

    add header and question 1

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("button#submit").click(function(event) {
+    event.preventDefault();
+    let python = 0;
+    let java = 0;
+    let c = 0;
+    let php = 0;
+    let cplus = 0;
+    let javascript = 0;
+    let csharp = 0;
+    let ruby = 0;
+    let objectiveC = 0;
+
+    
+  });
+})
